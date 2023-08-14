2b96ec7e60e9bbf1c27fbf869dbd8e165b715791
vgvishal538 <vgvishal538@gmail.com>
2023-08-14 12:51:10 +0530

    changes

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,7 +4,7 @@ const AboutUs = () => {
     return (
         <div className="content">
             <h1>About Us</h1>
-            <p>This is the about us page content.</p>
+            <p>This is the about us page .</p>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div className="content">
-            <h1>Welcome to our Bulletin News Blog</h1>
+            <h1>Welcome to our Bulletin News </h1>
             <p>This is the home page content.</p>
         </div>
     );
