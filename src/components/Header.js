8fd722f76ede6f19661e0ff9c79c998cb37a1fc4
vgvishal538 <vgvishal538@gmail.com>
2023-08-29 17:59:31 +0530

    news feed added

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className="header">
             <nav className="nav">
-                <Link to="/">Home</Link>
+                <Link to="/">Feed</Link>
                 <Link to="/privacy-policy">Privacy Policy</Link>
                 <Link to="/about-us">About Us</Link>
             </nav>
